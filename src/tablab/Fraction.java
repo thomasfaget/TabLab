c0d64979700b7f8128df7e9499829360e5375ac5
Thomas Faget <thomas.faget@hotmail.fr>
2018-03-14 10:05:12 -0400

    Add simple fraction

---
+++ src/tablab/Fraction.java
@@ -0,0 +1,56 @@
+package tablab;
+
+import java.math.BigInteger;
+
+class Fraction {
+    private BigInteger numerator;
+    private BigInteger denominator;
+
+    Fraction(int numerator, int denominator) {
+        if(denominator == 0) {
+            throw new IllegalArgumentException("denominator is zero");
+        }
+        if(denominator < 0) {
+            numerator *= -1;
+            denominator *= -1;
+        }
+        this.numerator = BigInteger.valueOf(numerator);
+        this.denominator = BigInteger.valueOf(denominator);
+        simplify();
+    }
+
+    public double doubleValue() {
+        return numerator.doubleValue() / denominator.doubleValue();
+    }
+
+    public boolean equal(Fraction fraction) {
+        long t = numerator.longValue() * fraction.denominator.longValue();
+        long f = fraction.numerator.longValue() * denominator.longValue();
+        return t==f;
+    }
+
+    public void multiply(Fraction fraction) {
+        this.numerator = numerator.multiply(fraction.numerator);
+        this.denominator = denominator.multiply(fraction.denominator);
+        simplify();
+    }
+
+    public void add(Fraction fraction) {
+        this.numerator = denominator.multiply(fraction.numerator).add(numerator.multiply(fraction.denominator));
+        this.denominator = denominator.multiply(fraction.denominator);
+        simplify();
+    }
+
+    private void simplify() {
+        if (numerator.intValue() != 0) {
+            BigInteger gcd = numerator.gcd(denominator);
+            numerator = numerator.divide(gcd);
+            denominator = denominator.divide(gcd);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
