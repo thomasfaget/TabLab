bbb58c4a6af876bc90f0fec0e78d412fad73e403
Thomas Faget <thomas.faget@hotmail.fr>
2018-04-02 20:35:55 -0400

    Add doc and comments

--- src/tablab/PartitionPlayer/PartitionPlayer.java
+++ src/tablab/PartitionPlayer/PartitionPlayer.java
@@ -10,8 +10,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class PartitionPlayer {
 
+    private MusicPartition partition;
+
     // The thread running the player
-    private Thread thread;
+    private Thread thread = new Thread(new PlayerRunnable());
 
     // State of the player
     private boolean isStarted;
@@ -28,67 +30,96 @@ public class PartitionPlayer {
      * @param partition the partition to play
      */
     public void playPartition(MusicPartition partition) {
+        this.partition = partition;
         stopPlaying();
-
         isStarted = true;
         isPaused = false;
-        PlayerRunnable playerRunnable = new PlayerRunnable(partition);
-        thread = new Thread(playerRunnable);
+        // start playing
         thread.start();
     }
 
+    /** Stop to play the partition.
+     */
     public void stopPlaying() {
         isStarted = false;
+        // unlock the thread if the player is paused
         if (isPaused) {
             synchronized (pauseLock) {
                 pauseLock.notify();
             }
         }
+        // interrupt the thread if not paused
         else if (thread != null) {
             thread.interrupt();
         }
 
+        // Call the callbacks
         for (PlayerCallback callback : callbacks) {
             callback.onStop();
         }
     }
 
+    /** Pause the partition, with saving the current position in the partition.
+     */
     public void pausePlaying() {
-        if (!isPaused) {
+        if (!isPaused && thread != null) {
             isPaused = true;
             thread.interrupt();
         }
 
+        // Call the callbacks
         for (PlayerCallback callback : callbacks) {
             callback.onPause();
         }
     }
 
+    /** Resume the partition, at the current position.
+     */
     public void resumePlaying() {
         isPaused = false;
+        // notify the lock
         if (thread != null) {
             synchronized (pauseLock) {
                 pauseLock.notify();
             }
+            // Call the callbacks
             for (PlayerCallback callback : callbacks) {
                 callback.onResume();
             }
         }
     }
 
+    /**
+     * Return true if the playing is playing.
+     * "Playing" means the player was started and was not stopped or finished to play, a player can be playing and paused.
+     * @return if the playing is playing
+     */
     public boolean isPlaying() {
         return isStarted;
     }
 
+
+    /**
+     * Return true if the playing is paused.
+     * A player can be paused and still playing.
+     * @return if the playing is paused
+     */
     public boolean isPaused() {
         return isPaused;
     }
 
-
+    /**
+     * Add a PlayerCallback to the player.
+     * @param callback the callback to add
+     */
     public void addPlayerCallback(PlayerCallback callback) {
         callbacks.add(callback);
     }
 
+    /**
+     * Remove a PlayerCallback to the player.
+     * @param callback the callback to remove.
+     */
     public void removePlayerCallback(PlayerCallback callback) {
         callbacks.remove(callback);
     }
@@ -96,27 +127,23 @@ public class PartitionPlayer {
 
     private class PlayerRunnable implements Runnable {
 
-        private MusicPartition partition;
-
-        PlayerRunnable(MusicPartition partition) {
-            this.partition = partition;
-        }
-
         @Override
         public void run() {
 
             int nbBars = partition.getMusicBars().size();
             int nbBeats = partition.getSettings().notesNumber;
             float timeBetweenNoteValue = 60000 / partition.getSettings().tempo; // The time (in ms) between 2 notes
+            List<NoteDelay> noteDelays = new ArrayList<>(); // A list with the time of all the notes
 
-            List<NoteDelay> noteDelays = new ArrayList<>();
-
+            // Compute the time (time value) of all the notes in the partition
             for (int bar = 1; bar <= nbBars; bar++) {
                 for (int beat = 1; beat <= nbBeats; beat++) {
                     int nbNotes = partition.getMusicBar(bar).getBeatStructure(beat).size();
+                    // Get the time evolution of the beat :
                     List<Fraction> fractions = partition.getMusicBar(bar).getBeatStructure(beat).getFractionEvolution(partition.getSettings());
                     for (int note = 1; note <= nbNotes; note++) {
                         float time;
+                        // Compute the real time of the note :
                         if (note < nbNotes) {
                             time = (float) (fractions.get(note).doubleValue() - fractions.get(note - 1).doubleValue());
                         } else {
@@ -128,12 +155,13 @@ public class PartitionPlayer {
                 }
             }
 
+            // Call the callbacks
             for (PlayerCallback callback : callbacks) {
                 callback.onStart();
             }
 
+            // Start playing the partition
             int i = 0;
-
             while (isStarted && i != noteDelays.size()) {
 
                 try {
@@ -142,15 +170,19 @@ public class PartitionPlayer {
                         NoteDelay noteDelay = noteDelays.get(i);
 
                         long currentTime = System.currentTimeMillis();
+                        // Call the callback wiht the current position of the partition
                         for (PlayerCallback callback : callbacks) {
                             callback.onNextNote(noteDelay.barNumber, noteDelay.beatNumber, noteDelay.noteNumber);
                         }
 
+                        // Sleep with the given time :
                         float sleepTime = noteDelay.delay - (System.currentTimeMillis() - currentTime);
                         Thread.sleep((long) sleepTime, (int) (1000 * (sleepTime - (long) sleepTime)));
                     }
                 }
+                // Handle interruption (pause and stop)
                 catch (InterruptedException e) {
+                    // pause the partition (lock)
                     if (isPaused) {
                         synchronized (pauseLock) {
                             try {
@@ -160,6 +192,7 @@ public class PartitionPlayer {
                             }
                         }
                     }
+                    // Stop, or error : stop playing
                     else {
                         isStarted = false;
                     }
@@ -167,12 +200,16 @@ public class PartitionPlayer {
             }
 
             isStarted = false;
+            // Call the callbacks
             for (PlayerCallback callback : callbacks) {
                 callback.onFinish();
             }
         }
     }
 
+    /**
+     * Data structure which contains the current position in the partition (bar, beat and note), the time of this 'position' (i.e. a note (pitch value))
+     */
     private class NoteDelay {
 
         float delay;
