2010ef58fb3e654a3b90d79385dbb8eef7a94031
Thomas Faget <thomas.faget@hotmail.fr>
2018-04-03 14:58:22 -0400

    A Runnable partition player

---
+++ src/tablab/PartitionPlayer/SimplePartitionPlayer.java
@@ -0,0 +1,32 @@
+package tablab.PartitionPlayer;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * A simple implementation of PartitionPlayer and AbstractPartitionPlayer.
+ * This class use a ReentrantLock for the mutex and the method 'sleep' of 'Thread' to sleep on the current thread.
+ */
+public class SimplePartitionPlayer extends AbstractPartitionPlayer {
+
+    private final Lock lock = new ReentrantLock(true);
+
+    @Override
+    void sleep(long millis, int nano) throws InterruptedException {
+        Thread.sleep(millis, nano);
+    }
+
+    @Override
+    void lockPause() throws InterruptedException {
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
+
+    @Override
+    void notifyPause() {
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+}
